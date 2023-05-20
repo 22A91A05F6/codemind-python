e60db342775fa79ce6257c3f34b0b9a207089945
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2023-05-20 22:32:11 +0530

    def countWords(str):     count = 1     for i in range(1, len(str) - 1):         if (str[i].isupper()):             count += 1
        return count
     
    # Driver code
    str = str(input())
    print(countWords(str))

---
+++ Camel_case_word_count.py
@@ -0,0 +1,12 @@
+def countWords(str):
+    count = 1
+    for i in range(1, len(str) - 1):
+        if (str[i].isupper()):
+            count += 1
+ 
+    return count
+ 
+# Driver code
+str = str(input())
+print(countWords(str))
+ 
