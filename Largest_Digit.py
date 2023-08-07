5c1039fefb6350314a700bdf84da5c1a98fa9dbf
Vijaya Lakshmi <121787485+22A91A05F6@users.noreply.github.com>
2023-08-07 09:41:17 +0530

    n=int(input()) s=0 while n>0:     r=n%10     if s<r:         s=r     n=n//10 print(f"{s}")

--- Largest_Digit.py
+++ Largest_Digit.py
@@ -1,11 +1,8 @@
 n=int(input())
-z=len(str(n))
-x=0
-for i in range(z):
-    if(n%10>x):
-       x=n%10
-       n=n//10
-    else:
-        n=n//10
-print(x)
-    
+s=0
+while n>0:
+    r=n%10
+    if s<r:
+        s=r
+    n=n//10
+print(f"{s}")
